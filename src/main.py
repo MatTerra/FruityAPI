5ed0d6e0d96fd9a168e926269768f5ae9d565202
Mateus Berardo de Souza Terra <mateus.b.s.terra@gmail.com>
2023-08-25 12:07:23 -0300

    Add CORS

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 
 import firebase_admin
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from firebase_admin import credentials
 
 from infra.repository.species_sql_repository import SpeciesSQLDAO
@@ -12,11 +13,19 @@ repository_container = RepositoryContainer()
 repository_container.wire(packages=["core", "infra"])
 TEST = bool(os.getenv("TEST"))
 if (bool(os.getenv("CREATE_DB"))):
-    SpeciesSQLDAO().create_table_if_not_exists()
+    dao = SpeciesSQLDAO()
+    dao.create_table_if_not_exists()
 
 cred = credentials.Certificate('firebase_cred.json')
 firebase_admin.initialize_app(cred)
 
 app = FastAPI()
+origins = ["http://localhost:57951",
+           "https://fruity.matterra.com.br"]
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
 app.container = repository_container
 app.include_router(species_v1_router)
