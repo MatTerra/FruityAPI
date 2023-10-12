5689084c38987eabb08c342c7150fb6bcbaa4506
Mateus Berardo de Souza Terra <mateus.b.s.terra@gmail.com>
2023-10-12 16:45:44 -0300

    Add a bunch'o logs

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+print("ABOUT TO START")
 import os
 
 import firebase_admin
@@ -12,20 +13,29 @@ from infra.routers.tree_router import tree_v1_router
 from infra.routers.health_router import health_v1_router
 from infra.repository import RepositoryContainer
 
+print("IMPORT DONE")
+
 repository_container = RepositoryContainer()
 repository_container.wire(packages=["core", "infra"])
+
+print("CREATED REPOSITORY CONTAINER")
 TEST = bool(os.getenv("TEST"))
 if bool(os.getenv("CREATE_DB")):
+    print("CREATE DB")
     dao = SpeciesSQLDAO()
     dao.create_table_if_not_exists()
     dao.close()
     dao = TreeSQLDAO()
     dao.create_table_if_not_exists()
     dao.close()
+    print("CREATED DB")
 
+print("STARTING FIREBASE")
 cred = credentials.Certificate('/secrets/firebase_cred.json')
 firebase_admin.initialize_app(cred)
+print("STARTED FIREBASE")
 
+print("STARTING APP")
 app = FastAPI()
 origins = ["http://localhost:57951",
            "https://fruity.matterra.com.br"]
@@ -38,3 +48,4 @@ app.container = repository_container
 app.include_router(species_v1_router)
 app.include_router(tree_v1_router)
 app.include_router(health_v1_router)
+print("STARTED APP")
